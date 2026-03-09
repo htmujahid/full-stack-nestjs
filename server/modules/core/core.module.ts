603b4cd373bf01ae798486a829e59e060559f467
htmujahid <htmujahid@gmail.com>
2026-03-09 22:09:06 +0500

    Remove ConfigModule from CoreModule, simplifying the module structure and improving application organization.

--- server/modules/core/core.module.ts
+++ server/modules/core/core.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [ConfigModule],
-  exports: [ConfigModule],
 })
 export class CoreModule {}
