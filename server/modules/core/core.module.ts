365bf93c07cee1c770b6994061da858b8599edcc
htmujahid <htmujahid@gmail.com>
2026-03-10 15:19:21 +0500

    Add ThrottleModule for rate limiting functionality and implement RateLimit entity with database storage support

--- server/modules/core/core.module.ts
+++ server/modules/core/core.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ThrottleModule } from './throttle/throttle.module';
 
 @Module({
+  imports: [ThrottleModule],
 })
 export class CoreModule {}
--- server/modules/identity/identity.module.ts
+++ server/modules/identity/identity.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MeModule } from './me/me.module';
-import { ThrottleModule } from './throttle/throttle.module';
 
 @Module({
-  imports: [ThrottleModule, UserModule, AuthModule, MeModule],
+  imports: [UserModule, AuthModule, MeModule],
   exports: [UserModule, AuthModule],
 })
 export class IdentityModule {}
