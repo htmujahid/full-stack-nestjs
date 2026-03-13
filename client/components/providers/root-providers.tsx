caa1841d3fee82472b045a9440b475bd16c1e2e8
htmujahid <htmujahid@gmail.com>
2026-03-13 07:16:38 +0500

    add shadcn components

---
+++ client/components/providers/root-providers.tsx
@@ -0,0 +1,11 @@
+import { Toaster } from "../ui/sonner";
+import { TooltipProvider } from "../ui/tooltip";
+
+export function RootProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <TooltipProvider>
+      {children}
+      <Toaster />
+    </TooltipProvider>
+  )
+}
