48a8d6330dc24b736f5bdf95fd75dce672f6cba1
htmujahid <htmujahid@gmail.com>
2026-03-13 07:05:41 +0500

    Add Tailwind CSS integration and update styles in main component

--- client/main.tsx
+++ client/main.tsx
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello World that is amazing</h1>
+    <h1 className="text-3xl font-bold">Hello World that is amazing</h1>
   </StrictMode>,
 );
